bf6de1ef84e924c803add83e2cda9a59bbd1cbfe
Dmitry Travnikov <travnikov@outlook.com>
2026-06-01 00:12:51 +0300

    header.js is finished

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,1 +1,7 @@
 import { cLog } from '../../index.js';
+
+let headerHamburger = document.querySelector('.header__hamburger');
+
+headerHamburger.onclick = function () {
+  this.classList.toggle('active');
+};
