5894d44ba4e8cd93d80bc3a122744e24d179ae29
Dmitry Travnikov <travnikov@outlook.com>
2026-06-02 00:09:59 +0300

    components are deleted and bootstrap testing is starting

--- src/components/header/header.js
+++
@@ -1,7 +0,0 @@
-import { cLog } from '../../index.js';
-
-let headerHamburger = document.querySelector('.header__hamburger');
-
-headerHamburger.onclick = function () {
-  this.classList.toggle('active');
-};
